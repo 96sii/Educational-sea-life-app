3b3b351778b70af05f8324290beb8a2c0295f1ca
Stefan Hone <stefan.hone@live.co.uk>
2021-11-15 14:51:31 +0000

    install react-image-gallery npm package, setup demo on Animal page

---
+++ client/src/components/MyGallery.js
@@ -0,0 +1,18 @@
+import ImageGallery from 'react-image-gallery';
+import './MyGallery.css';
+
+
+const MyGallery = ({items}) => {
+    
+    if(!items) {
+        return null;
+    }
+
+    return (
+        <>
+            <ImageGallery items = {items}/>
+        </>
+    )
+}
+
+export default MyGallery;
